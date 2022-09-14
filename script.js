2af5f1d1dfec28d78358bff30faa0b3657221708
RobThompson <rob.thomp15@gmail.com>
2022-09-13 19:02:37 -0500

    added eventListner to submit button and created function that grabs temp by city searched, and appends to box on pag

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-var APIKEy = e79ba8556f84815ec979fab05d8e3f
-let cityName;
+var APIKEy = "94e79ba8556f84815ec979fab05d8e3f";
+var savedUl = $('.list-group')
+var fetchButton = $('.btn-primary')
+var cityName = $('#city-name')
+var tempBox = $(".box");
 
-letQueryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid={API key}";
-fetch(queryURL)
+
+
+
+function getApi() {
+  var cityNameValue = cityName.val()
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityNameValue + '&APPID=' + APIKEy;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.main.temp);
+
+      tempBox.empty();
+      var temp = $('<h3>');
+      temp.text(data.main.temp);
+
+      tempBox.append(temp)
+
+    });
+}
+fetchButton.on('click', getApi);
